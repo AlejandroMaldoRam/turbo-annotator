ee34ac22b45f86a4ac1acd1e5b84b606cdbd71ae
Alejandro Maldonado <alejandro.maldonado@ciiia.mx>
2023-12-31 00:47:19 -0600

    Add method for displaying table with annotations

--- app/main-app-v2.py
+++ app/main-app-v2.py
@@ -82,7 +82,7 @@ app.layout = html.Div([
                     dbc.Col([
                     dbc.Card(
                         dbc.CardBody([
-                            html.H4("About the object"),
+                            html.H4("About the task"),
                             html.Div(id='obj-info-div')
                         ])
                     )
@@ -98,7 +98,8 @@ app.layout = html.Div([
     dcc.Store(id='labels-storage'),
     dcc.Store(id='current-img-storage'),
     dcc.Store(id='current-obj-storage'),
-    dcc.Store(id='annotations-storage')
+    dcc.Store(id='annotations-storage'),
+    dcc.Store(id='current-labels-storage')
 ])
 
 #-------------BACKEND---------------
@@ -112,11 +113,28 @@ def resize_strip_to_width(image, width):
     else:
         return image
 
+def count_labels(annotations_list, key):
+    labels = []
+    #print("count: ", annotations_list)
+    for d in annotations_list['detections']:
+        if key in d:
+            labels.append(d[key])
+    return labels
+
+def get_summarized_labels(labels_list):
+    labels_dict = {}
+    for l in labels_list:
+        if l in labels_dict:
+            labels_dict[l] += 1
+        else:
+            labels_dict[l] = 1
+    return labels_dict
 
 @app.callback(
-    Output('image-info-div', 'children'),
+    Output('image-info-div', 'children', allow_duplicate=True),
     Output('detections-storage', 'data'),
     Output('annotations-storage', 'data', allow_duplicate=True),
+    #Output('current-labels-storage', 'data', allow_duplicate=True),
     Input('folder-input', 'value'),
     prevent_initial_call=True
 )
@@ -126,12 +144,14 @@ def open_folder(folder_addr):
         detections_list = json.load(f)
         f.close()
         n_images = len(detections_list)
+        #labels_list = []
 
         annotations_path = "/".join(folder_addr.split("/")[:-1])+"/annotations.json"
         if os.path.exists(annotations_path):
             with open(annotations_path, 'r') as f:
                 annotations_list = json.load(f)
                 print("Annotations files opened!")
+                #labels_list = count_labels(annotations_list, "class_name")
         else:
             annotations_list = detections_list
             print("There are no previous annotations")
@@ -147,12 +167,13 @@ def open_folder(folder_addr):
 
 @app.callback(
     Output('current-img-div', 'children'),
-    Output('obj-info-div', 'children'),
+    Output('image-info-div', 'children', allow_duplicate=True),
     Output('img-progress', 'value'),
     Output('current-img-storage', 'data'),
+    Output('current-labels-storage', 'data', allow_duplicate=True),
     Input('next-img-button', 'n_clicks'),
     Input('prev-img-button', 'n_clicks'),
-    State('detections-storage', 'data'),
+    State('annotations-storage', 'data'),
     prevent_initial_call=True
 )
 def display_image(n_clicks_next, n_clicks_prev, detections_json):
@@ -183,18 +204,24 @@ def display_image(n_clicks_next, n_clicks_prev, detections_json):
             jpg_as_text = base64.b64encode(buffer.tobytes())
             dataURI = 'data:image/jpeg;base64,' + str(jpg_as_text, 'ascii')
             progress_img = int(index/n_images*100)
+
+            labels_list = []
+            labels_list = count_labels(detections_list[index], "class_name")
             
             # ensamble results
             return [[html.P("Image {}".format(detections_list[index]['image_addr'])),
                     html.Img(src=dataURI)], 
-                    [html.H5("Detected objects: "),
-                        html.P("{}".format(len(detections)))],
+                    [html.H5("Images in dataset: "),
+                    html.P("{}".format(len(detections_list))),
+                    html.H5("Detected objects: "),
+                    html.P("{}".format(len(detections)))],
                     progress_img,
-                    json.dumps([index])]
+                    json.dumps([index]),
+                    json.dumps(labels_list)]
         else:
-            return ["No images", "No objects", 0, json.dumps([-1])]
+            return ["No images", "No objects", 0, json.dumps([-1]), None]
     else:
-        return ["No images", "No objects", 0, json.dumps([-1])]
+        return ["No images", "No objects", 0, json.dumps([-1]), None]
 
 
 @app.callback(
@@ -202,13 +229,15 @@ def display_image(n_clicks_next, n_clicks_prev, detections_json):
     Output('obj-progress', 'value'),
     Output('current-obj-storage', 'data'),
     Output('label-p', 'children', allow_duplicate=True),
+    Output('obj-info-div', 'children'),
     Input('next-obj-button', 'n_clicks'),
     Input('prev-obj-button', 'n_clicks'),
     State('annotations-storage', 'data'),
     State('current-img-storage', 'data'),
+    State('current-labels-storage', 'data'),
     prevent_initial_call=True
 )
-def display_object(n_clicks_next, n_clicks_prev, detections_json, current_img_json):
+def display_object(n_clicks_next, n_clicks_prev, detections_json, current_img_json, current_labels_json):
     if detections_json:
         detections_list = json.loads(detections_json)
         if len(detections_list)>0:
@@ -240,37 +269,49 @@ def display_object(n_clicks_next, n_clicks_prev, detections_json, current_img_js
             jpg_as_text = base64.b64encode(buffer.tobytes())
             dataURI = 'data:image/jpeg;base64,' + str(jpg_as_text, 'ascii')
             progress_obj = int(index/n_objects*100)
+
+            labels_list = json.loads(current_labels_json)
+            results_dict = get_summarized_labels(labels_list)
+            if len(results_dict)>0:
+                results_table = dash_table.DataTable([{"Producto":k, "Cantidad": v} for k, v in list(results_dict.items())])
+            else:
+                results_table = "No labels"
             
             # ensamble results
-            return [html.Img(src=dataURI, height=600), progress_obj, json.dumps([index]), current_label]
+            return [html.Img(src=dataURI, height=600), progress_obj, json.dumps([index]), current_label, results_table]
         else:
-            return ["No objects", 0, json.dumps([-1]), "Current label: ---"]    
+            return ["No objects", 0, json.dumps([-1]), "Current label: ---", 'No info']    
     else:
-        return ["No objects", 0, json.dumps([-1]), "Current label: ---"]
+        return ["No objects", 0, json.dumps([-1]), "Current label: ---", 'No info']
     
 @app.callback(
     Output('label-p', 'children', allow_duplicate=True),
     Output('annotations-storage', 'data', allow_duplicate=True),
+    Output('current-labels-storage', 'data', allow_duplicate=True),
     Input('label-dropdown', 'value'),
     State('annotations-storage', 'data'),
     State('current-img-storage', 'data'),
     State('current-obj-storage', 'data'),
+    State('current-labels-storage', 'data'),
     prevent_initial_call=True
 )
-def label_object(value, annotations_json, current_img_json, current_obj_json):
+def label_object(value, annotations_json, current_img_json, current_obj_json, current_labels_json):
     if annotations_json:
         current_img = json.loads(current_img_json)[0]
         current_obj = json.loads(current_obj_json)[0]
         annotations_list = json.loads(annotations_json)
+        labels_list = json.loads(current_labels_json)
         if current_img != -1 and current_obj != -1:
             print("Value: ", value)
-            annotations_list[current_img]['detections'][current_obj]['class_id'] = int(value.split('|')[0])
-            annotations_list[current_img]['detections'][current_obj]['class_name'] = value
-            return ["Current label: {}".format(value), json.dumps(annotations_list)]    
+            if value:
+                annotations_list[current_img]['detections'][current_obj]['class_id'] = int(value.split('|')[0])
+                annotations_list[current_img]['detections'][current_obj]['class_name'] = value
+                labels_list.append(value)
+            return ["Current label: {}".format(value), json.dumps(annotations_list), json.dumps(labels_list)]    
         else:
-            return ["Current label: --", json.dumps(annotations_list)]    
+            return ["Current label: --", json.dumps(annotations_list), json.dumps(labels_list)]    
     else:
-        return ["Current label: --", None]
+        return ["Current label: --", None, None]
     
 @app.callback(
     Output('last-saved-p', 'children'),
@@ -295,5 +336,6 @@ def save_annotations(n_clicks_img, n_clicks_obj, annotations_json, folder_addr):
     else:
         return "Last saved: Never"
 
+# TODO: Add a table with all the labeled objects in the annotation file. For this I need to add a storge for the labeld objects
 if __name__ == '__main__':
     app.run(debug=True)
