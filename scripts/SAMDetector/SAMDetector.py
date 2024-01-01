c3a3e64acb7fb5eaddd3f32b569a65a3cd9ed3a8
Alejandro Maldonado <alejandro.maldonado@ciiia.mx>
2023-12-31 18:57:17 -0600

    Add methods for detecting objects using prompts

--- scripts/SAMDetector/SAMDetector.py
+++ scripts/SAMDetector/SAMDetector.py
@@ -10,20 +10,23 @@ class SAMDetector:
     def __init__(self, sam_model_dir, sam_model_type, device='cuda') -> None:
         self.initialize_model(sam_model_dir, sam_model_type, device)
 
-    """
-    This method initialze SAM.
-    """
     def initialize_model(self, sam_model_dir, sam_model_type, device='cuda'):
+        """
+        This method initialze SAM.
+        """
         # Initialize SAM
         self.sam = sam_model_registry[sam_model_type](checkpoint=sam_model_dir)
         self.sam.to(device=device)
         self.mask_generator = SamAutomaticMaskGenerator(self.sam)
+        self.predictor = SamPredictor(self.sam)
 
-    """
-    This function returns a list of dictionaries with the detected objects.
-    It will return only the rotated rect and the bounding box.
-    """
+
     def detect(self, image):
+        """
+        This function returns a list of dictionaries with the detected objects.
+        It will return only the rotated rect and the bounding box.
+        """
+
         # Get masks
         masks = self.mask_generator.generate(image)
         h,w,c = image.shape
@@ -58,6 +61,61 @@ class SAMDetector:
 
         return results
     
+    def set_image(self, image):
+        """
+        This method set the image and calculates the encoding.
+        """
+        self.predictor.set_image(image)
+
+    def predict(self, point_coords, point_labels, box, multimask_output):
+        """
+        This method use the prompt to get the objects.
+        """
+        results = []
+        masks, scores, logits = self.predictor.predict(
+                point_coords=point_coords,
+                point_labels=point_labels,
+                box=box,
+                multimask_output=multimask_output,
+                )
+        
+        
+        #print("masks: \n", masks.shape)
+        
+        for i in range(masks.shape[0]):
+            mask = masks[i,:,:]
+            #print("mask:\n", mask.shape)
+            bin_mask = np.zeros(mask.shape, dtype=np.uint8)
+            bin_mask[mask] = 255
+            contours, hierarchy = cv2.findContours(bin_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            
+            if len(contours)>1:
+                max_area_rect = 0
+                contour = contours[0]    
+                for c in contours:
+                    area = cv2.contourArea(c)
+                    if area > max_area_rect:
+                        contour = c
+            else:
+                contour = contours[0]
+
+            rotated_rect = cv2.minAreaRect(contour)
+            rect = cv2.boundingRect(contour)
+            #print("rect: ", rect)
+            #print("rrect: ", rotated_rect)
+            area = rect[2]*rect[3]
+            result = {'rotated_rect':rotated_rect, 'rect': rect, 'min_area': area}
+            results.append(result)
+        
+        # sort rectangles by area
+        results = sorted(results, key=lambda x: x['min_area'], reverse=True)
+        for i in range(len(results)):
+            results[i]['id'] = i
+
+        return results
+        
+
+    
     """
     This function draws the detected objects in the input image
     """
@@ -66,9 +124,9 @@ class SAMDetector:
         BB_COLOR_2 = (13,112,218)
         BB_FONT = cv2.FONT_HERSHEY_DUPLEX
         FONT_SCALE = 0.5
-        print(results)
+        #print(results)
         for r in results:
-            print(r)
+            #print(r)
             rect = r['rotated_rect']
             #score = r[2]
             #BB_COLOR = BB_COLOR_1 if score>0.5 else BB_COLOR_2
